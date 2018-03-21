9d3ff9ed30e375013328b1ebb8d7885285e1ea20
Leo Simmons <leooscar.simmons@gmail.com>
2018-03-20 23:45:18 -0700

    manage_dotfiles function distributes from and collects into dotfiles repo

--- convenience_commands.sh
+++ convenience_commands.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
-copy_dotfiles(){
+manage_dotfiles(){
 
     declare -a dotfile_names=(
-        ~/.bashrc
-        ~/.vimrc
-        ~/.Xmodmap
-        ~/.gitconfig
+        .bashrc
+        .vimrc
+        .Xmodmap
+        .gitconfig
     )
 
-    echo "copying dotfiles to dotfiles repo"
+    action=$1
+
+    if [[ $action == "collect" ]]; then
+        echo "collecting dotfiles into dotfiles repo"
+    elif [[ $action == "distribute" ]]; then
+        echo "distributing dotfiles from repo"
+    fi
+
     for file_name in "${dotfile_names[@]}"
     do
         echo $file_name
-        cp $file_name ~/projects/dotfiles/
+        if [[ $action == "collect" ]]; then
+            cp ~/$file_name ~/projects/dotfiles/$file_name
+        elif [[ $action == "distribute" ]]; then
+            cp ~/projects/dotfiles/$file_name ~/$file_name
+        fi
     done
 
+    # terminator config is special because it doesn't go directly
+    # in home dir like the other files
     terminator_config_file=~/.config/terminator/config
     echo $terminator_config_file
-    cp $terminator_config_file ~/projects/dotfiles/terminator_config
+    if [[ $action == "collect" ]]; then
+        cp $terminator_config_file ~/projects/dotfiles/terminator_config
+    elif [[ $action == "distribute" ]]; then
+        cp ~/projects/dotfiles/terminator_config $terminator_config_file
+    fi
+
 }
 
-alias copydot="copy_dotfiles"
+alias dotcol="manage_dotfiles collect"
+alias dotdis="manage_dotfiles distribute"
