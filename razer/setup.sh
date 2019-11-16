c99431aa4a442490d31f67d547e1580a2f167c09
Leo Simmons <leooscar.simmons@gmail.com>
2019-11-16 15:57:12 -0800

    install chrome/spotify/slack via cli, add docker group, other things

--- razer/setup.sh
+++ razer/setup.sh
@@ -13,7 +13,9 @@ sudo apt-get -y install\
         gnupg-agent \
         software-properties-common\
         build-essential\
-        chrome-gnome-shell
+        chrome-gnome-shell\
+        python3-venv\
+        gnome-tweaks
 
 # apt-transport-https -> software-properties-common for docker
 
@@ -28,6 +30,9 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
 
 # TERMINATOR
 mkdir /home/leo/.config/terminator
@@ -53,6 +58,19 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl https://raw.githubusercontent.com/lsimmons2/dotfiles/master/.vimrc > ~/.vimrc
 
+# CHROME
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# SPOTIFY
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+# SLACK
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+sudo apt-get install ./slack-desktop-4.0.2-amd64.deb
+
 # KEY REPEAT AND DELAY
 gsettings set org.gnome.desktop.peripherals.keyboard delay 165
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
@@ -64,26 +82,10 @@ curl https://raw.githubusercontent.com/lsimmons2/dotfiles/master/.gitconfig > ~/
 curl https://raw.githubusercontent.com/lsimmons2/dotfiles/razer//master/.bashrc > ~/.bashrc
 curl https://raw.githubusercontent.com/lsimmons2/dotfiles/razer/master/.bash_profile > ~/.bash_profile
 
-# FOR TRACKPAD
-sudo apt remove xserver-xorg-input-synaptics
-sudo apt install xserver-xorg-core
-sudo apt install xserver-xorg-input-libinput
-
-# DISABLE SUPER-* DOCK SHORTCUTS
-dconf write /org/gnome/shell/extensions/dash-to-dock/hot-keys "false"
-
-# SHORTCUTS FOR CHANGING WORKSPACES
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>0']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>1']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>2']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>3']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>4']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>5']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>6']"
-
-
 
 # MANUALLY
-# download chrome, spotify, slack
 # make dock small, on the bottom, hidden
 # make workspace switching animation fast - https://extensions.gnome.org/extension/1328 / https://askubuntu.com/a/1083797/725311
+# solarized
+# node/yarn install doesn't work
+# copy keys and make ~/.ssh/config
