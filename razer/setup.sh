3e96861ba3402a1b8ee74c69e9af1491d2474a40
Leo Simmons <leooscar.simmons@gmail.com>
2020-01-18 12:29:48 -0800

    changes to update script

--- razer/setup.sh
+++ razer/setup.sh
@@ -1,3 +1,7 @@
+# SETUP DIR
+mkdir -p ~/setup
+cd ~/setup
+
 # APT-GET STUFF
 sudo apt-get update
 sudo apt-get upgrade
@@ -15,19 +19,17 @@ sudo apt-get -y install\
         build-essential\
         chrome-gnome-shell\
         python3-venv\
-        gnome-tweaks
+        gnome-tweaks\
+        tree
 
 # apt-transport-https -> software-properties-common for docker
-
-# DO EVERYTHING IN HOME DIR FOR NOW
-cd 
 
 # DOCKER
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
@@ -55,7 +57,7 @@ curl https://raw.githubusercontent.com/lsimmons2/dotfiles/master/razer/.Xmodmap 
 
 # VIM
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl https://raw.githubusercontent.com/lsimmons2/dotfiles/master/.vimrc > ~/.vimrc
 
 # CHROME
@@ -81,6 +83,37 @@ curl https://raw.githubusercontent.com/lsimmons2/dotfiles/master/.gitconfig > ~/
 # BASHRC/PROFILE
 curl https://raw.githubusercontent.com/lsimmons2/dotfiles/razer//master/.bashrc > ~/.bashrc
 curl https://raw.githubusercontent.com/lsimmons2/dotfiles/razer/master/.bash_profile > ~/.bash_profile
+
+
+enables gpu support for docker containers... which comes by default with docker 19.03
+sudo apt-get install -y docker nvidia-container-toolkit
+
+# CUDA DRIVERS/TOOLKIT
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-1-local-10.1.243-418.87.00/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+# NVIDIA DOCKER
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu18.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+# PREY
+wget https://downloads.preyproject.com/prey-client-releases/node-client/1.9.2/prey_1.9.2_amd64.deb
+sudo dpkg -i prey_1.9.2_amd64.deb # will error out due to missing dependencies
+sudo apt-get -f -y install # updates the missing dependencies for prey and prey itself
+
+# RIPGREP
+wget https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
 
 
 # MANUALLY
