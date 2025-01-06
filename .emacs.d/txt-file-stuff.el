443a5b9d859122605cb0730901b0159949c2fe80
Leo Simmons <leooscar.simmons@gmail.com>
2025-01-06 00:17:18 -0500

    some improvements to folding in .txt files

--- .emacs.d/txt-file-stuff.el
+++ .emacs.d/txt-file-stuff.el
@@ -7,8 +7,10 @@
   (setq-local evil-shift-width 4)   ;; Set shift width to 4 (for Evil mode indentation)
 
   ;; Folding
-  (setq-local outline-regexp "^[ \t]*") ;; Use indentation for folding
-  (outline-minor-mode 1)                ;; Enable folding for text-mode
+  (setq-local outline-regexp "^[ \t]*[^ \t\n]") ;; Match only non-blank lines with leading spaces
+  (setq-local outline-blank-line t)             ;; Prevent blank lines from being part of the fold
+  (outline-minor-mode 1)                       ;; Enable outline minor mode
+
 
   ;; Wrapping and indentation
   (setq-local word-wrap t)               ;; Enable word wrapping
@@ -27,6 +29,15 @@
                     (evil-insert-line)
                     (insert ">")
                     (evil-normal-state)))
-  )
+
+
+  (add-hook 'outline-view-change-hook
+            (lambda ()
+              (dolist (ov (overlays-in (point-min) (point-max)))
+                (when (eq (overlay-get ov 'invisible) 'outline)
+                  (overlay-put ov 'display
+                               (propertize "  <folded>"
+                                           'face 'font-lock-string-face)))))
+            nil t)) ;; Use buffer-local hook
 
 (add-hook 'text-mode-hook #'my-text-mode-setup)
